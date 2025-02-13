98acb5912b6725e94791f887b3cf6dafaff20c20
bckben <131319923+bckben@users.noreply.github.com>
2025-02-12 17:00:25 -0700

    Add files via upload

---
+++ Models/Movie.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission6_Keeney.Models
+{
+    public class Movie
+    {
+        [Key] public int MovieId { get; set; }
+
+        [Required] public string Title { get; set; } 
+
+        [Required] public string Genre { get; set; } 
+
+        [Required] public string Director { get; set; }
+
+        [Required] public int ReleaseYear { get; set; }
+
+        [Required] public string Rating { get; set; } 
+
+        public bool? Edited { get; set; }
+        public string? LentTo { get; set; }
+
+        [StringLength(25)] public string? Notes { get; set; }
+    }
+}
