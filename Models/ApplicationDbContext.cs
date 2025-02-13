b12214d7cc9bded9dc87e0774fd8b412d81f8eb8
bckben <131319923+bckben@users.noreply.github.com>
2025-02-12 18:21:46 -0700

    Delete Models directory

--- Models/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using Mission6_Keeney.Models;
-
-public class ApplicationDbContext : DbContext
-{
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
-    public DbSet<Movie> Movies { get; set; }
-}
